b7eb3a1544b4f976397e454a571b8174924b6088
Muhammad Moiz Arshad <43073474+muhammadmoiza@users.noreply.github.com>
2019-07-18 17:16:01 +0500

    Update elastic_search.py

--- elastic_search.py
+++ elastic_search.py
@@ -19,13 +19,11 @@ if col_labels == '':
 col_labels = str(col_labels).split(' ')
 
 no_cols = len(col_labels)
-print(no_cols)
 if no_cols == 0:
 	exit(1)
 
 logs = [line.strip() for line in open(filename)]
 
-#cols = ['month', 'day', 'time', 'host', 'protocol']
 index1 = input("Enter name for new index\n>\t")
 doc_type1 = input("Enter type of document\n>\t")
 
@@ -42,7 +40,6 @@ for i in logs:
 
 	content = '{"'
 	for j in range(len(fields)):
-		#print(len(fields))
 		if j < no_cols:
 			content += col_labels[j] + '":"' + fields[j]
 		else:
@@ -50,7 +47,6 @@ for i in logs:
 		if j < len(fields) - 1:
 			content += '","'
 	content += '"}'
-	#print(content)
 	es.index(index=index1, doc_type=doc_type1, id=j, body=content)
 	j += 1
 print("Logs indexed.\n")
